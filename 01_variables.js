360d3f5d614cf8b808aa5f02d872ea93bae64dea
Syed Hoque <syedcs36@gmail.com>
2025-10-11 10:28:48 +0530

    1st java script practice code

---
+++ 01_variables.js
@@ -0,0 +1,24 @@
+
+const accountId=12345;
+let accountEmail="Syedcs@gmail.com" 
+var accountPassword="12345"
+accountCity="Jaipur"
+let acocuntState 
+
+
+accountEmail ="syedcs106@gmail.com";
+accountPassword=9000;
+accountCity="assam"
+
+//console.log(accountCity)
+console.table([accountId,accountEmail,accountPassword,accountCity,acocuntState])
+
+/**
+ * Please do not use var 
+ * because of issue in block and functional scope ,as var data types can be updated out side block or function
+ * 
+ */
+
+
+
+
---
+++ 02_dataTypes.js
@@ -0,0 +1,2 @@
+//alert (3 +3) //we are using node js not browser 
+let age =18 
