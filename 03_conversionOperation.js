1c1b647bd21ec7bcc4609c5b43466d1796584b9c
Syed Hoque <syedcs36@gmail.com>
2025-10-11 11:09:06 +0530

    master

---
+++ 03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score= "33a"
+
+console.log(typeof(score))
+
+let getScore= String (score)
+
+console.log(typeof(score))
+console.log(getScore)
+
+
+let getScore1= Number (score)
+console.log(getScore1)
