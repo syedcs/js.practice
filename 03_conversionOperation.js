92e8ec52f3911d14efe743f606ee98588f9bd266
Syed Hoque <syedcs36@gmail.com>
2025-10-20 18:32:50 +0530

    updated code with string conversion

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,6 +1,6 @@
-let score= "33a"
+ let score= "33a"
 
-console.log(typeof(score))
+ console.log(typeof(score))
 
 let getScore= String (score)
 
@@ -10,3 +10,8 @@ console.log(getScore)
 
 let getScore1= Number (score)
 console.log(getScore1)
+
+let newNumber=45
+let  abc =String(newNumber)
+
+console.log(abc)
