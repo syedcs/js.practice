596ccacd4f12be33ad31ea87aedfd9b318d18b68
Syed Hoque <syedcs36@gmail.com>
2025-10-23 23:16:06 +0530

    code with string & number

---
+++ 06_string.js
@@ -0,0 +1,4 @@
+const name="Syed";
+let repoCount =50;
+
+console.log(`Hi my name is ${name} and my git repo count is ${repoCount} and thanks ` )
---
+++ 07_number_math.js
@@ -0,0 +1,11 @@
+/* const score =100
+console.log(score)
+
+const num = new Number(400)
+console.log(num)
+
+console.log(num.toString().length)
+console.log(num.toFixed(4)) */
+
+console.log(Math)
+console.log(Math.random())
