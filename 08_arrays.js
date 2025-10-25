eccec6b42cb97386413908bfb09d0062b96084cc
Syed Hoque <syedcs36@gmail.com>
2025-10-25 13:07:53 +0530

    code with Arrys

---
+++ 08_arrays.js
@@ -0,0 +1,18 @@
+const myarr1= [0,4,6,"history", 8]
+const myarr2=new Array(1,4,6,7)
+
+
+const mynewarr1= myarr1.splice(0,1)
+console.log(mynewarr1)
+
+console.log(myarr1)
+console.log(myarr2)
+
+console.log(myarr1[3])
+
+myarr1.push("syed")
+myarr2.pop()
+
+console.log(myarr1)
+console.log(myarr2)
+
